5ed6b3e97be8037354712bb62cf29769e5d27f30
Travis Jeffery <tj@travisjeffery.com>
2013-10-09 11:55:36 -0500

    Add lib header file

---
+++ TRVSEventSource/TRVSEventSource.h
@@ -0,0 +1,15 @@
+//
+//  TRVSEventSource.h
+//  TRVSEventSource
+//
+//  Created by Travis Jeffery on 10/9/13.
+//  Copyright (c) 2013 Travis Jeffery. All rights reserved.
+//
+
+#ifndef TRVSEventSource_TRVSEventSource_h
+#define TRVSEventSource_TRVSEventSource_h
+
+#import "TRVSServerSentEvent.h"
+#import "TRVSEventSourceSessionManager.h"
+
+#endif
