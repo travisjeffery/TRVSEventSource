6e8db3c8342d0ca100c05f2bd4ba0464fe2fb066
Travis Jeffery <tj@travisjeffery.com>
2013-10-10 23:01:17 -0500

    Stream legit events

--- TRVSEventSourceExample/server.js
+++ TRVSEventSourceExample/server.js
@@ -1,11 +1,17 @@
 var http = require('http')
   , interval = 2000 
   , port = 8000
+  , id = 0
   
 http.createServer(function(req, res){
   res.writeHead(200, {'Transfer-Encoding': 'chunked', 'Content-Type': 'text/event-stream'})
   setInterval(function(){
-    var event = 'event: test\ndata: {"text":"' + (new Date().getTime()) + '"}\n\n'
+    var event = 'event: message\ndata: {"id": ' + (++id) + ', "body":"' + (new Date().getTime()) + '", "author_id": 1, "conversation_id": 1}\n\n'
     res.write(event)
   }, interval)
 }).listen(port)
+
+console.log('He makes sweet music with the enameled stones,\n' +
+  'Giving a gentle kiss to every sedge,\n' +
+  'He overtaketh in his pilgrimage.\n\n' +
+  'Streaming events on port', 8000)
