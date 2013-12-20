798334a59f31a556bafd13db32cc74a00b530804
Travis Jeffery <tj@travisjeffery.com>
2013-12-20 03:45:21 -0500

    More docs

--- TRVSEventSource/TRVSEventSourceDelegate.h
+++ TRVSEventSource/TRVSEventSourceDelegate.h
@@ -13,8 +13,34 @@
 
 @protocol TRVSEventSourceDelegate <NSObject>
 @optional
+
+/**
+ *  Lets the delegate know that the event source has opened its connection.
+ *
+ *  @param eventSource The event source.
+ */
 - (void)eventSourceDidOpen:(TRVSEventSource *)eventSource;
+
+/**
+ *  Lets the delegate know that the event source has closed its connection.
+ *
+ *  @param eventSource The event source.
+ */
 - (void)eventSourceDidClose:(TRVSEventSource *)eventSource;
+
+/**
+ *  Lets the delegate know that the event source has received an event.
+ *
+ *  @param eventSource The event source.
+ *  @param event       The received event.
+ */
 - (void)eventSource:(TRVSEventSource *)eventSource didReceiveEvent:(TRVSServerSentEvent *)event;
+
+/**
+ *  Lets the delegate know that the event source failed.
+ *
+ *  @param eventSource The event source.
+ *  @param error       The error associated with the failure.
+ */
 - (void)eventSource:(TRVSEventSource *)eventSource didFailWithError:(NSError *)error;
 @end
